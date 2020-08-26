5233ff133f785a613e56c502074645aa515e286a
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-26 14:19:11 -0500

    undefined

---
+++ null_undefined.js
@@ -0,0 +1,11 @@
+
+// undefined
+let name;
+console.log(name);
+
+function add(num1, num2){
+    //console.log(num1 + num2);
+    return num1 + num2; // if I don't assign anything with return it will show undefined
+}
+let result = add(4); // if don't match with parameters it will show undefined
+console.log(result);
