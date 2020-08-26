670dfe2991fae9736e971b0d906dcaac8073c400
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-26 13:21:04 -0500

    true vs fasle

---
+++ true-false.js
@@ -0,0 +1,10 @@
+// false 
+// condition will be false if value is 0, NaN, null, undefined, empty string 
+let name = "";
+
+if(name){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
