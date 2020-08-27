05481cd0dfbae5cd3bdf80f968621e0650ce6123
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-26 20:36:40 -0500

    double and thriple equal

--- true-false.js
+++ true-false.js
@@ -8,3 +8,16 @@ if(name){
 else {
     console.log("condition is false");
 }
+// 1 is related with true and 0 is related with false
+// "==" double equal only check value and "===" three equal will check value and data type
+// thats === using three equal always better in javascript 
+const first = 2;
+const second = "2";
+
+
+if( first === second){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
