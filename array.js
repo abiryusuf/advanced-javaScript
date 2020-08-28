066202a2fdecbabbf9ff341533901275af7a383a
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-28 10:30:48 -0500

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+const nums =[2,3,4,5,6,7,8,9];
+//slice
+const result = nums.slice("slice", 1,4);
+console.log(result);
+
+//splice
+const str = nums.splice("splice", 1,4);
+console.log(str);
+
+//join
+const join = nums.join( " ", 12, 13);
+console.log(join);
