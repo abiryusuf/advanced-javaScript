4d7e2033ea634e44f7444b54f4be550b4180a89a
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-05 23:09:13 -0500

    array List

--- array.js
+++ array.js
@@ -1,3 +1,5 @@
+// JavaScript arrays are used to store multiple values in a single variable.
+
 const nums =[2,3,4,5,6,7,8,9];
 //slice
 const result = nums.slice("slice", 1,4);
@@ -9,6 +11,29 @@ console.log(str);
 
 //join
 const join = nums.join( " ", 12, 13);
-console.log(join);
+console.log("join", join);
 
 console.log(nums.indexOf(3))
+
+var name = "abir";
+var res = name.split("");
+console.log(res)
+
+var array = ["A", "B", "C", "Abir", "Mim"];
+array[0] = "F";
+console.log(array);
+console.log(array.sort())
+
+var lastArray = array[array.length - 1];
+console.log("Last Aarry", lastArray);
+
+function arrayDemo(arr){
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        const lastArray = arr[arr.length -1 -i ]
+
+        
+    }
+    return lastArray
+}
+console.log(arrayDemo(array));
